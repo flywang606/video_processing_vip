1435de71c37a525b1c0761aa076e21347e848316
flywang606 <flywang606@163.com>
2021-01-09 16:10:29 +0800

    add test image

---
+++ cmodel/include/video_in_out.h
@@ -0,0 +1,2 @@
+
+int gen_rgb_memory_bin(char *file_in);
--- cmodel/src/test.c
+++ cmodel/src/test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "video_in_out.h"
 
 int main()
 {
-	
+	char *file="./../origin_image/lena_64_64.ppm";
+    gen_rgb_memory_bin(file);
 }
